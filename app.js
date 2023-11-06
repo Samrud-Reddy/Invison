02c2a61e2ad4123b12a703045c328b4bdc6b36cb
Samrud Reddy <samrudr@gmail.com>
2023-11-06 17:11:40 +0530

    first commit finished navbar v1

--- app.js
+++ app.js
@@ -20,6 +20,35 @@ app.get('/', (req, res) => {
   res.render('index.ejs')
 })
 
+//event routs
+app.get("/event/sports/cricket", (req, res) => {
+  res.send("/event/sports/cricket")
+})
+app.get("/event/sports/football", (req, res) => {
+  res.send("/event/sports/football")
+})
+app.get("/event/sports/basketball", (req, res) => {
+  res.send("/event/sports/basketball")
+})
+app.get("/event/sports/swiming", (req, res) => {
+  res.send("/event/sports/swiming")
+})
+app.get("/event/ba/dance", (req, res) => {
+  res.send("/event/ba/dance")
+})
+app.get("/event/ba/music", (req, res) => {
+  res.send("/event/ba/music")
+})
+app.get("/event/ba/art", (req, res) => {
+  res.send("/event/ba/art")
+})
+app.get("/event/ba/martial arts", (req, res) => {
+  res.send("/event/ba/martial arts")
+})
+app.get("/event/ba/photography", (req, res) => {
+  res.send("/event/ba/photography")
+})
+
 app.use((req, res) => {
   res.status(404)
   res.render("pagenotfound.ejs")
